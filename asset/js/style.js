b9d72646ae0924dcf8a7223f8ce01fb3c32bce64
John _D <43427114+jdiscalsote@users.noreply.github.com>
2024-02-01 10:53:21 +0800

    Update style.js

--- asset/js/style.js
+++ asset/js/style.js
@@ -1,3 +1,24 @@
 function toggleMenu(x) {
     x.classList.toggle("change");
 }
+
+/* jQuery(function ($) {
+    $(window).resize(function () {
+        if (window.matchMedia('(max-width: 575px)').matches) {
+            $(".offcanvasSide").addClass("offcanvasMobile");
+        }
+        else {
+            $(".offcanvasSide").removeClass("offcanvasMobile");
+        }
+    });
+
+    $(document).load($(window).width(changeClass));
+    function changeClass() {
+        if (window.matchMedia('(max-width: 575px)').matches) {
+            $(".offcanvasSide").addClass("offcanvasMobile");
+        }
+        else {
+            $(".offcanvasSide").removeClass("offcanvasMobile");
+        }
+    }
+}); */
