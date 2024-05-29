6f6685f3b30e3944c6ce2e1183bcacec8bbe91ef
John Nicole Discalsote <john.n.discalsote@accenture.com>
2024-05-30 01:00:20 +0800

    UI Improvement, Skills Added, JS Updated

--- asset/js/style.js
+++ asset/js/style.js
@@ -1,10 +1,19 @@
 function toggleMenu(x) {
     x.classList.toggle("change");
-}
+};
 
-document.addEventListener('contextmenu', event => event.preventDefault());
+// Add event listeners to navigation links to close the offcanvas menu
+document.querySelectorAll('.navbar-nav li a').forEach(link => {
+    link.addEventListener('click', function() {
+        let offcanvasElement = document.querySelector('#canvasMenu');
+        let offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
+        if (offcanvasInstance) {
+            toggleMenu();
+        }
+    });
+});
 
-/* jQuery(function ($) {
+jQuery(function ($) {
     $(window).resize(function () {
         if (window.matchMedia('(max-width: 575px)').matches) {
             $(".offcanvasSide").addClass("offcanvasMobile");
@@ -23,4 +32,6 @@ document.addEventListener('contextmenu', event => event.preventDefault());
             $(".offcanvasSide").removeClass("offcanvasMobile");
         }
     }
-}); */
+});
+
+document.addEventListener('contextmenu', event => event.preventDefault());
